21671916d086bd7d1dab9206f1c38e3a3dd2ddea
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-21 19:05:24 +0800

    Created helper functions
    - Resolve data file path to training data set
    - Resolve raw weights
    - Export onnx func to set standardised output args

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+from .resolve import resolve_data, resolve_raw_weight
+from .export_to_onnx import export_to_onnx
+
+__all__ = [
+    "resolve_data",
+    "resolve_raw_weight",
+    "export_to_onnx"
+]
